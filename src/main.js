07d464ae4c5826ea20bbd743f60285be871e9479
Hjj <1607710122@qq.com>
2019-12-24 01:22:15 +0800

    提交了IconFont 图标的选定。

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import store from './store.js'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 import '@/assets/iconfont/iconfont.css' // 引入字体样式
 import '@/assets/iconfont/iconfont.js'  // 引入symbol样式
 
 Vue.config.productionTip = false
+Vue.use(Vant);
 
 new Vue({
   router,
